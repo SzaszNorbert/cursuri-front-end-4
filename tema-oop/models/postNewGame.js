bd9cc048c1951aa25913f88a3433e774be2b2098
SzaszNorbert <you@example.com>
2018-05-20 15:00:53 +0300

    tema-oop post content addition

---
+++ tema-oop/models/postNewGame.js
@@ -0,0 +1,44 @@
+window.addEventListener("load",onLoaded);
+var gameUrlPost='https://games-world.herokuapp.com/games';
+function onLoaded(){
+	var title=document.querySelector("input[name='title']");
+
+	var genre=document.querySelector("input[name='genre']");
+
+	var publisher=document.querySelector("input[name='publisher']");
+
+	var image=document.querySelector("input[name='image']");
+
+	var description=document.querySelector("[name='description']");
+
+	var form=document.querySelector("form");
+	form.addEventListener("submit",function(event){
+		event.preventDefault();
+
+		createNewPost(title.value,genre.value,publisher.value,image.value,description.value);
+	})
+}
+
+function createNewPost(title,genre,publisher,image,description){
+	console.log(title,genre,publisher);
+	var dataPost={
+		title:title,
+		genre:genre,
+		publisher:publisher,
+		image:image,
+		description:description
+	};
+
+	console.log(dataPost);
+
+	$.ajax(gameUrlPost,{
+		type:'POST',
+		data:dataPost,
+		success:function(){
+			alert("Success!");
+		},
+		error:function(xhr){
+			alert("Error:"+xhr);
+		}
+	});
+}
--- tema-oop/views/gamesListView.js
+++ tema-oop/views/gamesListView.js
@@ -13,13 +13,19 @@ window.onload=function(){
 function createPosts(){
 			console.log(posts.itemList.length);
 			var containEl=document.getElementById("container");
+			var box=document.createElement('div');
 			for(var i=0;i<posts.itemList.length;i++){
 				var items=posts.itemList[i];//simplifying your variable
+				var picture=document.createElement('img');
+				picture.setAttribute('src',items.imageUrl);
+				picture.setAttribute('alt','404');
+				picture.classList.add('img-size');
 				var postEl=document.createElement('a');
 				postEl.setAttribute('href','file:///C:/cursuri-front-end-4/tema-oop/pages/gameDetails.html?gamePostId='+items._id);//?kulcs+valami igy id rendelhetunk a linkjeinkhez
 				postEl.setAttribute('target','blank');
 				postEl.innerHTML=items.title+'<br>';
 				containEl.appendChild(postEl);
+				containEl.appendChild(picture);
 			}
 		}
 //error function if something is messed up
