8ef38dab8ed7ea4ffa356073b54029232740e7f5
SzaszNorbert <you@example.com>
2018-05-27 14:39:57 +0300

    tema-oop styling1

---
+++ HigherOrderFunctions/script/flattering.js
@@ -0,0 +1,7 @@
+let array=[[1,2,3],[4,5],[6]];
+
+let fullArray=array.reduce((a,b)=>a.concat(b));
+
+console.log(fullArray);
+
+//resolved!!!
---
+++ HigherOrderFunctions/script/myLoop.js
@@ -0,0 +1,7 @@
+function loop(a,b,c,d){
+	for(let i=0;i<a;i++){
+		
+	}
+}
+
+loop(3,n=>n>0,n=>n-1;console.log);
